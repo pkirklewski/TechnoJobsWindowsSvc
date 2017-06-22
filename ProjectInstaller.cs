56bf2ef05bc21924229c35167baa210c0deb3de4
pkirklewski <kirklewskis@gmail.com>
2017-06-22 18:34:53 +0100

    Service installed - but events are all ZERO

--- ProjectInstaller.cs
+++ ProjectInstaller.cs
@@ -20,5 +20,19 @@ namespace TechnoJobService
         {
 
         }
-    }
+
+
+        protected override void OnBeforeInstall(IDictionary savedState) 
+        {
+            string parameter = "MySource1\" \"MyLogFile1"; 
+            Context.Parameters["assemblypath"] = "\"" + Context.Parameters["assemblypath"] + "\" \"" + parameter + "\""; 
+            base.OnBeforeInstall(savedState); 
+        }
+        
+    
+    } //end of public partial class ProjectInstaller
+
+
+
+
 }
